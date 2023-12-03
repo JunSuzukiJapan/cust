13394ac4bed84cecad35b9b981cb33a69612b789
JunSuzukiJapan <jun.suzuki.japan@gmail.com>
2023-12-03 11:37:27 +0900

    impl Display trait

--- lexer/src/tokenizer_error.rs
+++ lexer/src/tokenizer_error.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use super::Location;
 
 pub enum TokenizerError {
@@ -10,6 +11,36 @@ pub enum TokenizerError {
     IllegalEndOfInputWhileParsingCharAfter(Option<Location>, char),
     IllegalCharWhileParsingString(Option<Location>),
     IllegalEndOfInputWhileParsingString(Option<Location>),
+}
 
+impl TokenizerError {
+    pub fn get_location(&self) -> &Option<Location> {
+        match self {
+            Self::IllegalEndOfInput(opt_loc) => opt_loc,
+            Self::SyntaxError(opt_loc, _filename, _line, _column) => opt_loc,
+            Self::IllegalCharWhileParsingDigit(opt_loc) => opt_loc,
+            Self::IllegalCharWhileParsingChar(opt_loc) => opt_loc,
+            Self::IllegalCharWhileParsingCharAfter(opt_loc, _char) => opt_loc,
+            Self::IllegalEndOfInputWhileParsingChar(opt_loc) => opt_loc,
+            Self::IllegalEndOfInputWhileParsingCharAfter(opt_loc, _char) => opt_loc,
+            Self::IllegalCharWhileParsingString(opt_loc) => opt_loc,
+            Self::IllegalEndOfInputWhileParsingString(opt_loc) => opt_loc,
+        }
+    }
+}
 
+impl fmt::Display for TokenizerError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::IllegalEndOfInput(_opt_loc) => write!(f, "illegal end of input."),
+            Self::SyntaxError(_opt_loc, _filename, _line, _column) => write!(f, "syntax error."),
+            Self::IllegalCharWhileParsingDigit(_opt_loc) => write!(f, "illegal char while parsing digit."),
+            Self::IllegalCharWhileParsingChar(_opt_loc) => write!(f, "illegal char while parsing char."),
+            Self::IllegalCharWhileParsingCharAfter(_opt_loc, char) => write!(f, "illegal char while parsing char after '{}'", char),
+            Self::IllegalEndOfInputWhileParsingChar(_opt_loc) => write!(f, "illegal end of input while parsing char."),
+            Self::IllegalEndOfInputWhileParsingCharAfter(_opt_loc, char) => write!(f, "illegal end of input while parsing char after '{}'", char),
+            Self::IllegalCharWhileParsingString(_opt_loc) => write!(f, "illegal char while parsing string."),
+            Self::IllegalEndOfInputWhileParsingString(_opt_loc) => write!(f, "illegal end of input while parsing string."),
+        }
+    }
 }
