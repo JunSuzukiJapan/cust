1510cf07d1e3bc56518cdbc9633499cd0461fbdf
JunSuzukiJapan <jun.suzuki.japan@gmail.com>
2024-10-30 11:04:09 +0900

    impl: store to tuple element

--- code_gen/tests/tuple_test.rs
+++ code_gen/tests/tuple_test.rs
@@ -59,3 +59,34 @@ fn code_gen_define_tuple() {
     let f: JitFunction<FuncType_void_i32> = unsafe { gen.execution_engine.get_function("test").ok().unwrap() };
     assert_eq!(unsafe { f.call() }, 3);
 }
+
+
+#[test]
+fn code_gen_access_tuple_element() {
+    let src = "
+        int printf(char* format, ...);
+
+        int test(){
+            $<int, int> tpl = $(1, 2);
+            tpl.0 = 3;
+            tpl.1 = 4;
+
+            return tpl.0 + tpl.1;
+        }
+    ";
+
+    // parse
+    let asts = parse_from_str(src).unwrap();
+
+    // code gen
+    let context = Context::create();
+    let gen = CodeGen::try_new(&context, "test run").unwrap();
+
+    let mut env = Env::new();
+    for i in 0..asts.len() {
+        let _any_value = gen.gen_toplevel(&asts[i], &mut env, None, None).unwrap();
+    }
+
+    let f: JitFunction<FuncType_void_i32> = unsafe { gen.execution_engine.get_function("test").ok().unwrap() };
+    assert_eq!(unsafe { f.call() }, 7);
+}
