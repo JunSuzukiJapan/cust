7cbb2014fa7162b86a59482ce8b749c69f934527
JunSuzukiJapan <jun.suzuki.japan@gmail.com>
2025-05-10 08:56:14 +0900

    pass test for if-let

--- code_gen/tests/enum_test.rs
+++ code_gen/tests/enum_test.rs
@@ -87,7 +87,7 @@ fn code_gen_struct_type_enum2() {
     let f: JitFunction<FuncType_void_i32> = unsafe { gen.execution_engine.get_function("test").ok().unwrap() };
     assert_eq!(unsafe { f.call() }, 1);
 }
-/*
+
 #[test]
 fn code_gen_struct_type_enum() {
     let src = "
@@ -96,17 +96,19 @@ fn code_gen_struct_type_enum() {
             Zot {
                 int x;
                 int y;
+                int z;
             },
         };
 
-        int main() {
+        int test() {
             enum Foo x = Foo::Zot {
                 x: 1;
                 y: 2;
+                z: 3;
             };
 
-            if let (Foo::Zot { x, y } = x) {
-                return x + y;
+            if let (Foo::Zot { x, y, z } = x) {
+                return x + y + z;
             } else {
                 return 0;
             }
@@ -126,8 +128,6 @@ fn code_gen_struct_type_enum() {
         let _any_value = gen.gen_toplevel(&asts[i], &mut env, None, None).unwrap();
     }
 
-    let f: JitFunction<FuncType_i32_i32> = unsafe { gen.execution_engine.get_function("test").ok().unwrap() };
-    assert_eq!(unsafe { f.call(0) }, 2);
-    assert_eq!(unsafe { f.call(1) }, 4);
+    let f: JitFunction<FuncType_void_i32> = unsafe { gen.execution_engine.get_function("test").ok().unwrap() };
+    assert_eq!(unsafe { f.call() }, 6);
 }
-*/
