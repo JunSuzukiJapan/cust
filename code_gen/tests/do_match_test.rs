689f0822441ee35dfa7a81499f6094c37953932e
JunSuzukiJapan <jun.suzuki.japan@gmail.com>
2025-06-05 09:37:51 +0900

    add test 'code_gen_do_match_multi_at'

--- code_gen/tests/do_match_test.rs
+++ code_gen/tests/do_match_test.rs
@@ -98,3 +98,47 @@ fn code_gen_do_match_multi_int() {
     assert_eq!(unsafe { f.call(7) }, 37);
     assert_eq!(unsafe { f.call(10) }, 40);
 }
+
+#[test]
+fn code_gen_do_match_multi_at() {
+    let src = "
+        int test(int input){
+            int output = 0;
+            do match (input) {
+                x @ 0 | x @ 1 | x @ 2 => {
+                    output = 10 + x;
+                },
+                x @ 3 | x @ 4 | x @ 5 => {
+                    output = 20 + x;
+                },
+                _ => {
+                    output = 100 + input;
+                }
+            }
+
+            return output;
+        }
+    ";
+
+    // parse
+    let asts = parse_from_str(src).unwrap();
+
+    // code gen
+    let context = Context::create();
+    let gen = CodeGen::try_new(&context, "test run").unwrap();
+
+    let mut env = Env::new();
+    for i in 0..asts.len() {
+        let _any_value = gen.gen_toplevel(&asts[i], &mut env, None, None).unwrap();
+    }
+
+    let f: JitFunction<FuncType_i32_i32> = unsafe { gen.execution_engine.get_function("test").ok().unwrap() };
+    assert_eq!(unsafe { f.call(0) }, 10);
+    assert_eq!(unsafe { f.call(1) }, 11);
+    assert_eq!(unsafe { f.call(2) }, 12);
+    assert_eq!(unsafe { f.call(3) }, 23);
+    assert_eq!(unsafe { f.call(4) }, 24);
+    assert_eq!(unsafe { f.call(5) }, 25);
+    assert_eq!(unsafe { f.call(6) }, 106);
+    assert_eq!(unsafe { f.call(10) }, 110);
+}
