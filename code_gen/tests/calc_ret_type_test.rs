2b55660d3c3fa9898ac7a97ea45ddcea6e3326b0
JunSuzukiJapan <jun.suzuki.japan@gmail.com>
2025-06-09 18:07:07 +0900

    add a test

--- code_gen/tests/calc_ret_type_test.rs
+++ code_gen/tests/calc_ret_type_test.rs
@@ -10,8 +10,6 @@ use common::*;
 #[test]
 fn code_gen_for_ret_type() -> Result<(), CodeGenError> {
     let src = "
-        int printf(char* format, ...);
-
         int test() {
             for(int i = 0; i < 3; i++){
                 return 2;
@@ -34,6 +32,35 @@ fn code_gen_for_ret_type() -> Result<(), CodeGenError> {
 
     let f: JitFunction<FuncType_void_i32> = unsafe { gen.execution_engine.get_function("test").ok().unwrap() };
     assert_eq!(unsafe { f.call() }, 2);
+
+    Ok(())
+}
+
+#[test]
+fn code_gen_for_ret_type2() -> Result<(), CodeGenError> {
+    let src = "
+        void test() {
+            for(int i = 0; i < 3; i++){
+                return;
+            }
+        }
+    ";
+
+    // parse
+    let asts = parse_from_str(src).unwrap();
+
+    // code gen
+    let context = Context::create();
+    let gen = CodeGen::try_new(&context, "test run").unwrap();
+
+    let mut env = Env::new();
+
+    for i in 0..asts.len() {
+        let _any_value = gen.gen_toplevel(&asts[i], &mut env, None, None).unwrap();
+    }
+
+    let f: JitFunction<FuncType_void_void> = unsafe { gen.execution_engine.get_function("test").ok().unwrap() };
+    assert_eq!(unsafe { f.call() }, ());
 
     Ok(())
 }
