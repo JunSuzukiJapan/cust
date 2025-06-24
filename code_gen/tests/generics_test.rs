d28031af451da17d7e90df47dded0aa1814ca07b
JunSuzukiJapan <jun.suzuki.japan@gmail.com>
2025-06-24 15:31:52 +0900

    fix: change test result

--- code_gen/tests/generics_test.rs
+++ code_gen/tests/generics_test.rs
@@ -20,7 +20,7 @@ fn code_gen_option() -> Result<(), Box<dyn Error>> {
                 return x;
             }
 
-            return 0;
+            return 5;
         }
     ";
 
@@ -39,6 +39,43 @@ fn code_gen_option() -> Result<(), Box<dyn Error>> {
 
     let f: JitFunction<FuncType_void_i32> = unsafe { gen.execution_engine.get_function("test").ok().unwrap() };
     assert_eq!(unsafe { f.call() }, 10);
+
+    Ok(())
+}
+
+#[test]
+fn code_gen_option2() -> Result<(), Box<dyn Error>> {
+    let src = "
+        enum Option<T> {
+            Some<T>,
+            None,
+        };
+
+        int test(){
+            enum Option<int> opt = Option::None;
+            if let (Option::Some(x) = opt) {
+                return x;
+            }
+
+            return 5;
+        }
+    ";
+
+    // parse
+    let asts = parse_from_str(src).unwrap();
+
+    // code gen
+    let context = Context::create();
+    let gen = CodeGen::try_new(&context, "test run").unwrap();
+
+    let mut env = Env::new();
+
+    for i in 0..asts.len() {
+        let _any_value = gen.gen_toplevel(&asts[i], &mut env, None, None).unwrap();
+    }
+
+    let f: JitFunction<FuncType_void_i32> = unsafe { gen.execution_engine.get_function("test").ok().unwrap() };
+    assert_eq!(unsafe { f.call() }, 5);
 
     Ok(())
 }
